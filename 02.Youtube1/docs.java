f8d7ac0f4416009bdbb8ca23216966faf3b39c63
anhvurz90 <anhvurz90@gmail.com>
2016-12-31 14:50:01 +0700

    Add Youtube1 doc:#01.Introduction

---
+++ 02.Youtube1/docs.java
@@ -0,0 +1,39 @@
+NODE JS TUTORIAL FOR BEGINNERS
+https://www.youtube.com/watch?v=w-7RQ46RgxU&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp
+
+01.Introduction: {
+	- Node.js ACTUALLY is: {
+		+ A platform which allows us to run JS on a computer/ server.
+		+ Read, delete and update files.
+		+ Easily communicate with a database.
+	}
+	- Why is Node.js so Popular? {
+		+ It uses Javascript
+		+ Very fast ( runs on the V8 engine & uses non-blocking code)
+		+ Huge ecosystem of open source packages (npm)
+		+ Greate for real-time services ( like chats)
+	}
+	- What We will Learn... {
+		+ The inner workings of Node.js:
+			V8 engine
+			Modules
+			Event emitter
+			The file system
+		+ Creating a web server
+			Routing
+			Express
+			Templating
+		+ Make a Node.js application (todo app)
+	}
+	- Editor {
+		+ ATOM
+			UI Theme: Nonokai Tweaked
+			Syntax Theme: Atom Monokai
+	}
+	- Github: https://github.com/iamshaunjp/node-js-playlist
+	- What You Need to Know: {
+		+ JavaScript
+		+ HTML
+		+ A tiny bit about the command line
+	}
+}
