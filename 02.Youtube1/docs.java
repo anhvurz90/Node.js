1d6ea5a4a0c7ab2e210b9ad7d81a5ad049c92b78
anhvurz90 <anhvurz90@gmail.com>
2017-01-03 22:59:50 +0700

    Add Youtube1 doc:#12.Creating a Server

--- 02.Youtube1/docs.java
+++ 02.Youtube1/docs.java
@@ -265,4 +265,15 @@ https://www.youtube.com/watch?v=w-7RQ46RgxU&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4b
 		E.g. 172.24.86.76:3000
 	}
 }
-12.
+12.Creating a Server {
+	- app.js: {
+		var http = require("http");
+		var server = http.createServer(function(req, res) {
+			console.log("request was made: " + req.url);
+			res.writeHead(200, {"Content-Type": "text/plain"});
+			res.end("Hey ninjas");
+		});
+		server.listen(3000, "127.0.0.1");
+		console.log("yo dawgs, now listening to port 3000");
+	}
+}
