a031e7fcb006e22217938c88430083396a5bcb9c
anhvurz90 <anhvurz90@gmail.com>
2017-01-08 21:00:59 +0700

    Add Youtube1 doc:#20.The Node Package Manager

--- 02.Youtube1/docs.java
+++ 02.Youtube1/docs.java
@@ -378,3 +378,8 @@ https://www.youtube.com/watch?v=w-7RQ46RgxU&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4b
 		});
 	}
 }
+20.The Node Package Manager: {
+	- website: https://npmjs.com
+	- npm init
+	- npm install express
+}
