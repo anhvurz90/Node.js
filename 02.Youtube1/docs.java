676f11bba2d42457af5974915987041d1dfc9187
anhvurz90 <anhvurz90@gmail.com>
2017-01-10 14:15:48 +0700

    Add Youtube1 doc:#24.Express Route Params

--- 02.Youtube1/docs.java
+++ 02.Youtube1/docs.java
@@ -432,3 +432,8 @@ https://www.youtube.com/watch?v=w-7RQ46RgxU&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4b
 	}
 	nodemon app
 }
+24.Route Parameters: {
+	- app.get("/profile/:id", function(req, res) {
+		res.send("You requested to see a profile with the id of " + req.params.id);
+	});
+}
